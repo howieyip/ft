7f5d6b0ec48b27c9eac8d4e417b79615c0dab79a
howieyip <howieyip@foxmail.com>
2023-08-17 13:13:23 +0800

    🎨 chore: conf

--- 303698/fight.py
+++ 303698/fight.py
@@ -5,7 +5,7 @@ from utils.fight import start
 start({
     'log_file': '303698/logs/fight.log',
     'PORT' : 11111,
-    'BUY_LIST' : [[60, 15, 200*1000]],
+    'BUY_LIST' : [[60, 15, 300*1000]],
     'MAX_VOLUME' : 500*1000,
     'ADJUST_BUY_DICT' : {
         'rise': [2, 3, 0],
--- 320451/fight.py
+++ 320451/fight.py
@@ -6,7 +6,7 @@ start({
     'log_file': '320451/logs/fight.log',
     'PORT' : 11112,
     'BUY_LIST' : [[60, 15, 300*1000]],
-    'MAX_VOLUME' : 400*1000,
+    'MAX_VOLUME' : 300*1000,
     'ADJUST_BUY_DICT' : {
         'rise': [2, 3, 1],
         'fall': [2, 3, 2]
