aba727466feb8ac2a6c637a16574e39536587312
howieyip <howieyip@foxmail.com>
2023-08-18 13:38:32 +0800

    🎨 chore: datestr_to_timestamp

--- utils/fight.py
+++ utils/fight.py
@@ -161,10 +161,10 @@ def timestamp_to_datestr(time_stamp, format_string="%Y-%m-%d %H:%M:%S"):
 
 # 将时间字符串转换为10位时间戳，时间字符串默认为2017-10-01 13:37:04格式
 def datestr_to_timestamp(time_str, format_str="%Y-%m-%d %H:%M:%S", pattern=r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}'):
-    if re.fullmatch(pattern, time_str):
-        return datetime.strptime(time_str, format_str).timestamp()
-    else:
-        return time.time()
+    # if re.fullmatch(pattern, time_str):
+    return datetime.datetime.strptime(time_str, format_str).timestamp()
+    # else:
+    #     return time.time()
 
 
 def get_golden_line(a, b):
