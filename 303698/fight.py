9ef9822c2ff248887c01980ff748a00018e453ef
howieyip <howieyip@foxmail.com>
2023-08-21 09:33:37 +0800

    🎨 chore: MAX_VOLUME

--- 303698/fight.py
+++ 303698/fight.py
@@ -6,7 +6,7 @@ start({
     'log_file': '303698/logs/fight.log',
     'PORT' : 11111,
     'BUY_LIST' : [[60, 15, 300*1000]],
-    'MAX_VOLUME' : 500*1000,
+    'MAX_VOLUME' : 700*1000,
     'ADJUST_BUY_DICT' : {
         'rise': [2, 3, 0],
         'fall': [2, 3, 2]
