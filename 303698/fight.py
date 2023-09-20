7e759d28cab3b5707b89ee33e1a6810ba6162cf6
howieyip <478593686@qq.com>
2023-09-20 23:47:11 +0800

    fix: log_file

--- 303698/fight.py
+++ 303698/fight.py
@@ -1,9 +1,10 @@
 import sys
 sys.path.append('../ft')
 from utils.fight import start
+import datetime
 
 start({
-    'log_file': '303698/logs/fight.log',
+    'log_file': '303698/logs/fight.' + datetime.date.today().strftime('%Y-%m-%d') + '.log',
     'PORT' : 11111,
     'BULL_CODE': 'auto',
     'BUY_VOLUME' : 300e3,
--- 320451/fight.py
+++ 320451/fight.py
@@ -1,9 +1,10 @@
 import sys
 sys.path.append('../ft')
 from utils.fight import start
+import datetime
 
 start({
-    'log_file': '320451/logs/fight.log',
+    'log_file': '320451/logs/fight.' + datetime.date.today().strftime('%Y-%m-%d') + '.log',
     'PORT' : 11112,
     'BUY_VOLUME' : 200e3,
     'MAX_VOLUME' : 300e3,
