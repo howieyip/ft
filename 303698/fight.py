2b200a45ea080c4c259952d16c26c5f17f442583
howieyip <howieyip@foxmail.com>
2023-11-21 09:09:17 +0800

    ✅ test: BUY_VOLUME

--- 303698/fight.py
+++ 303698/fight.py
@@ -7,8 +7,8 @@ start({
     'log_file': '303698/logs/fight.' + datetime.date.today().strftime('%Y-%m-%d') + '.log',
     'PORT' : 11111,
     'BULL_CODE': 'auto',
-    'BUY_VOLUME' : 400e3,
-    'MAX_VOLUME' : 600e3,
+    'BUY_VOLUME' : 600e3,
+    'MAX_VOLUME' : 800e3,
     'ALLOW_ADD': True,
     'ADD_PRICE_DIFF': 0.005
 })
