83569f6dd826194bab3e9ac1c2ac33d55784215a
howieyip <478593686@qq.com>
2024-05-23 11:07:00 +0800

    feat: FIRST_ORDER_DIFF

--- utils/fight.py
+++ utils/fight.py
@@ -48,17 +48,18 @@ conf = {
 
     'AUTO_PLACE_ORDER': False,                      # 买入后是否自动挂单分批卖出，若是则下面的ORDER_LIST有效
     'ORDER_LIST': [                                 # 下单多少股以上（大的写前面），每单挂多少股，例如下单800k，分3档200k 200k 400k挂单
-        [800e3, 0, 200e3, 200e3, 200e3, 200e3],
-        [700e3, 0, 150e3, 150e3, 200e3, 200e3],
-        [600e3, 0, 150e3, 150e3, 150e3, 150e3],
-        [500e3, 0, 100e3, 100e3, 150e3, 150e3],
-        [400e3, 0, 100e3, 100e3, 100e3, 100e3],
-        [300e3, 0, 50e3, 50e3, 100e3, 100e3],
-        [200e3, 0, 50e3, 50e3, 50e3, 50e3],
-        [150e3, 0, 50e3, 0, 50e3, 50e3],
-        [100e3, 0, 50e3, 0, 50e3, 0]
+        [800e3, 200e3, 200e3, 200e3, 200e3],
+        [700e3, 150e3, 150e3, 200e3, 200e3],
+        [600e3, 150e3, 150e3, 150e3, 150e3],
+        [500e3, 100e3, 100e3, 150e3, 150e3],
+        [400e3, 100e3, 100e3, 100e3, 100e3],
+        [300e3, 50e3, 50e3, 100e3, 100e3],
+        [200e3, 50e3, 50e3, 50e3, 50e3],
+        [150e3, 50e3, 0, 50e3, 50e3],
+        [100e3, 50e3, 0, 50e3, 0]
     ],
-    'FALL_PRICE_MIN_DIFF': 0.005,                   # 最后一个卖单降档的最小值为第几档
+    'FIRST_ORDER_DIFF': 0.002,                      # 第一个卖单为第几档
+    'LAST_ORDER_DIFF': 0.005,                       # 最后一个卖单为第几档
 
     'AUTO_ADJUST_SELL': True,                       # 是否自动调整挂的卖单的价格，若是则下面的ADJUST_SELL_DICT有效
     'ADJUST_SELL_DICT': {
@@ -735,11 +736,11 @@ def _position_list_query(stock_type='', need_log=True):
                 if check_result == 'loss_bull':
                     log.info('loss_bull, code: %s, nominal_price: %s, cost_price: %s' % (item.code, item.nominal_price, item.cost_price))
                     glb['loss'][item.code] = True
-                elif len(glb['submitted_sell_bull_list']) > 0 and item.nominal_price < round(glb['submitted_sell_bull_list'][0].price - 0.002, 3):
+                elif len(glb['submitted_sell_bull_list']) > 0 and item.nominal_price < round(glb['submitted_sell_bull_list'][0].price - conf['FIRST_ORDER_DIFF'], 3):
                     # 当前价格比最高价低2格的时候，重新挂单
                     if item.nominal_price < round(glb['max_nominal_price'] - 0.001, 3):
                         log.info('loss_order, code: %s, nominal_price: %s, cost_price: %s' % (item.code, item.nominal_price, item.cost_price))
-                        auto_place_order(item.code, item.qty, item.nominal_price - 0.002)
+                        auto_place_order(item.code, item.qty, item.nominal_price - conf['FIRST_ORDER_DIFF'] - 0.001)
                 elif conf['TRY_RECOVERY'] and (check_result == 'not_ready' or check_result == 'bear'):
                     log.info('sell_bull, code: %s, nominal_price: %s, cost_price: %s' % (item.code, item.nominal_price, item.cost_price))
                     # sell_all(code=item.code, qty=item.qty)
@@ -749,11 +750,11 @@ def _position_list_query(stock_type='', need_log=True):
                 if check_result == 'loss_bear':
                     log.info('loss_bear, code: %s, nominal_price: %s, cost_price: %s' % (item.code, item.nominal_price, item.cost_price))
                     glb['loss'][item.code] = True
-                elif len(glb['submitted_sell_bear_list']) > 0 and item.nominal_price < round(glb['submitted_sell_bear_list'][0].price - 0.002, 3):
+                elif len(glb['submitted_sell_bear_list']) > 0 and item.nominal_price < round(glb['submitted_sell_bear_list'][0].price - conf['FIRST_ORDER_DIFF'], 3):
                     # 当前价格比最高价低2格的时候，重新挂单
                     if item.nominal_price < round(glb['max_nominal_price'] - 0.001, 3):
                         log.info('loss_order, code: %s, nominal_price: %s, cost_price: %s' % (item.code, item.nominal_price, item.cost_price))
-                        auto_place_order(item.code, item.qty, item.nominal_price - 0.002)
+                        auto_place_order(item.code, item.qty, item.nominal_price - conf['FIRST_ORDER_DIFF'] - 0.001)
                 elif conf['TRY_RECOVERY'] and (check_result == 'not_ready' or check_result == 'bull'):
                     log.info('sell_bear, code: %s, nominal_price: %s, cost_price: %s' % (item.code, item.nominal_price, item.cost_price))
                     # sell_all(code=item.code, qty=item.qty)
@@ -846,7 +847,7 @@ def auto_place_order(code, volume, price, batch=True, cancel=True):
             vol += volume_diff
         if vol == 0:
             continue
-        data = smart_sell(code, vol, price + 0.001 * i)
+        data = smart_sell(code, vol, price + conf['FIRST_ORDER_DIFF'] + 0.001 * (i - 1))
         if data is False:
             log.info('auto_place_order => smart_sell error')
         elif glb['move_position']:
@@ -921,7 +922,7 @@ def auto_adjust(delta_price, i, adjust_dict, submitted_type):
         if glb['almost_over'] or glb['loss'].get(data.code):
             fall_price = round(ask_price + (adjust_dict['fall'][2] - 1) * 0.001, 3)
         else:
-            fall_price = round(max(find_buy_price(data) + conf['FALL_PRICE_MIN_DIFF'], ask_price - 0.001), 3)
+            fall_price = round(max(find_buy_price(data) + conf['LAST_ORDER_DIFF'], ask_price - 0.001), 3)
     rise_condition = False
     fall_condition = False
     if submitted_type.find('bull') > -1:
