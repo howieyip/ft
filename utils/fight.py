a67c8fb54ea34e672e0846af0ddbc1742031c7fe
howieyip <howieyip@foxmail.com>
2025-06-09 10:15:17 +0800

    🐛 fix: get_order_price

--- utils/fight.py
+++ utils/fight.py
@@ -949,10 +949,10 @@ def get_order_price(price, index):
         order_price =  round(price + (math.pow(index, 2) + 5 * index) / 2 * 0.001, 3)
         return order_price
     order_price = round(price + index * conf['EVERY_ORDER_DIFF'], 3)
-    if order_price > 0.25:
-        return round(math.ceil(order_price * 200) / 200, 3)
     if order_price > 0.5:
         return round(math.ceil(order_price * 100) / 100, 3)
+    if order_price > 0.25:
+        return round(math.ceil(order_price * 200) / 200, 3)
     return order_price
 
 
