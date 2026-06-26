832c679eb2f12b5c9562a4d5ca6852c25d0d2f4a
howieyip <howieyip@foxmail.com>
2026-06-26 20:31:48 +0800

    🎨 chore: code

--- 303698/fight.py
+++ 303698/fight.py
@@ -17,7 +17,7 @@ start({
     # 'ADD_ORDER_DIFF': 0.004,
     # 'EVERY_ORDER_DIFF': 0.002,
     # 'NEED_LOSS': True,
-    'BULL_CODE': 'HK.59539',
+    'BULL_CODE': 'HK.69345',
     'ADD_ORDER_DIFF': 0.032,
     'EVERY_ORDER_DIFF': 0.016,
     'FAR_ORDER_LIST': [[40e3, 40e3]],
