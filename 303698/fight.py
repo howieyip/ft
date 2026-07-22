15e7bc4073cb0470f0bbd6e4aa5e217025d0526e
howieyip <howieyip@foxmail.com>
2026-07-22 22:59:08 +0800

    🎨 chore: ADD_ORDER_DIFF

--- 303698/fight.py
+++ 303698/fight.py
@@ -18,7 +18,7 @@ start({
     # 'EVERY_ORDER_DIFF': 0.002,
     # 'NEED_LOSS': True,
     'BULL_CODE': 'HK.69345',
-    'ADD_ORDER_DIFF': 0.04,
+    'ADD_ORDER_DIFF': 0.02,
     'EVERY_ORDER_DIFF': 0.02,
     'FAR_ORDER_LIST': [[20e3, 20e3]],
 })
